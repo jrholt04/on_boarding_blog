608c8ee23505af013b44fa77042d37af34001a47
jrholt26 <jrholt26@transy.edu>
2025-01-29 15:35:44 -0500

    Git Commit Part 2 finished

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+//File: about.js
+//Jackson Holt about page 
+//Awesome Inc 
+import * as React from 'react'
+import {Link} from 'gatsby'
+
+const AboutPage = () => {
+    return (
+    <div>
+        <h1>
+            About Me
+        </h1>
+        <p>
+            I am a junoir CS student at Transy. I also play soccer and I am the president of pike
+        </p>
+    </div>
+    )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
